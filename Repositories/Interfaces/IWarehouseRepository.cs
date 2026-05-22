fad043dbbee2301abd40220ec31bfd060eff9ea5
linademiri <159142830+linademiri@users.noreply.github.com>
2026-05-22 12:32:27 +0200

    Added warehouse module files

---
+++ Repositories/Interfaces/IWarehouseRepository.cs
@@ -0,0 +1,8 @@
+using Warehouse.Services.Interfaces;
+
+namespace Warehouse.Repositories.Interfaces
+{
+    public interface IWarehouseRepository : IGenericRepository<Warehouse.Models.Warehouse>
+    {
+    }
+}
