c099067b382ffecb3442b88b4aa6cf4720b22a08
linademiri <159142830+linademiri@users.noreply.github.com>
2026-06-08 22:52:06 +0200

    small fixes

--- Program.cs
+++ Program.cs
@@ -62,10 +62,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174")
               .AllowAnyHeader()
               .AllowAnyMethod()
-                 .AllowCredentials();
+              .AllowCredentials();
     });
 });
 
